0cad73ac9fa8ebd4bc5a87c5279bb228e6932cca
yuyan2023 <971670319@qq.com>
2023-12-17 14:34:04 +1000

    first commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+
+import { defineConfig } from "vite";
+import path from "path";
+ 
+export default defineConfig({
+    server: {
+        port: 8080,
+    },
+    resolve:{
+        alias:{
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+});
